918cdba1b7ac20b12d3178eb20572c9f481c1b50
Asif2714 <asifabdullah47@gmail.com>
2024-02-10 17:52:41 +0000

    removed unused code

--- backend/identify_app/urls.py
+++ backend/identify_app/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 urlpatterns = [
     path('predict/', views.predict_image, name='predict_image'),
-    # path('upload/', views.upload_image, name='upload_image'),
     path('test-get/', views.test_get_request, name='get_request'),
-    # Test urls
-    # path('test/', views.test, name='test'),
 ]
--- backend/identify_app/views.py
+++ backend/identify_app/views.py
@@ -4,7 +4,6 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 import os
-# from .forms import ImageUploadForm
 
 # PyTorch related imports
 import torch
@@ -20,12 +19,11 @@ from io import BytesIO
 
 
 
-@csrf_exempt  # For simplicity, disabling CSRF. For production, consider using CSRF tokens.
+@csrf_exempt  # TODO: exempt for simplicity, disabling CSRF. For production, we need to include tokens
 def upload_image(request):
     print(request.body)
     if request.method == 'POST':
-        # image_file = request.FILES.get('image', None)
-        data = request.body.decode('utf-8')  # Decode the raw bytes to a string
+        data = request.body.decode('utf-8') 
         json_data = json.loads(data)
         image_data = json_data.get('image')
 
@@ -81,71 +79,15 @@ def test_get_request(request):
         return JsonResponse({'error': 'Invalid request method'}, status=405)
 
 
-# # # Template views
-# # def test(request):
-# #     return render(request, 'test.html')
-
-# import base64
-# from io import BytesIO
-
-# @csrf_exempt
-# def predict_image(request):
-#     if request.method == 'POST':
-#         try:
-#             data = json.loads(request.body)
-#             img_base64 = data.get('imgsource')
-
-#             if not img_base64:
-#                 return JsonResponse({'error': 'No image provided'}, status=400)
-
-#             # Decode the base64 string
-#             img_data = base64.b64decode(img_base64)
-#             img = Image.open(BytesIO(img_data)).convert('RGB')
-
-#             # Process the image
-#             input_img = preprocess_img(img)
-#             with torch.no_grad():
-#                 output = model(input_img)
-#                 probabilities = F.softmax(output, dim=1)
-#                 confidence, predicted_class = probabilities.max(1)
-
-#                 if confidence.item() > 0.1:
-#                     predicted_class_string = str(predicted_class.item())
-#                     if predicted_class_string in ordered_species_json:
-#                         classification = ordered_species_json[predicted_class_string]['plant_name']
-#                         return JsonResponse({'Predicted Class': classification, 'Confidence': confidence.item()})
-#                     else:
-#                         return JsonResponse({'error': 'Predicted class not in JSON'}, status=404)
-#                 else:
-#                     return JsonResponse({'error': 'Confidence too low'}, status=200)
-#         except Exception as e:
-#             return JsonResponse({'error': str(e)}, status=500)
-#     else:
-#         return JsonResponse({'error': 'Invalid request method'}, status=405)
-
-# def preprocess_img(image):
-#     preprocess = transforms.Compose([
-#         transforms.Resize((224, 224)),
-#         transforms.ToTensor(),
-#         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-#     ])
-#     input_tensor = preprocess(image)
-#     input_batch = input_tensor.unsqueeze(0)
-#     return input_batch
-
-
 @csrf_exempt
 def predict_image(request):
     print("predicting image")
-    # print("Full request data:", request)
     print("Full request data body:", request.body)
     if request.method == 'POST':
         print("in post")
         image_file = request.FILES.get('file')
         print(request.FILES)
 
-        # file_type = imghdr.what(image_file)
-        # print(file_type) 
 
         if not image_file:
             print("no image!")
