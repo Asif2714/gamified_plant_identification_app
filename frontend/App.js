3104cd0b76fa210db380750a1686a4bfc7a62d64
Asif2714 <asifabdullah47@gmail.com>
2024-02-14 00:21:20 +0000

    initial navigation setup

--- frontend/App.js
+++ frontend/App.js
@@ -1,99 +1,78 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+// import XPBar from "./components/XPBar";
+import { View, Text, StyleSheet } from 'react-native';
 
-import React, { useState } from 'react';
-import { View, Button, Image, Alert, StyleSheet,Text } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import axios from 'axios';
+// Importing the screens for navigation
+import TakeImageScreen from "./screens/TakeImageScreen";
+import HomeScreen from "./screens/HomeScreen.js";
+import ChallengesScreen from "./screens/ChallengesScreen";
+import LeaderboardGamesScreen from "./screens/LeaderboardGamesScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
-import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
-import { Platform } from 'react-native';
+const Tab = createBottomTabNavigator();
 
-const options = {
-  title: 'Select Image',
-  type: 'library',
-  options: {
-    selectionLimit: 1,
-    mediaType: 'photo',
-    includeBase64: false,
-}
-}
-
-const ipAddress = '10.47.5.125';
-
-export default function App() {
-
-  const [image, setImage] = useState(null);
-
-  const pickImageAndSend = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.Images,
-          allowsEditing: true,
-          aspect: [4, 3],
-          quality: 1,
-        });
-
-        const formdata = new FormData()
-
-        //TODO: remove logging
-        const image = result.assets[0];
-        console.log("This is image" + image);
-
-        formdata.append('file', {
-          uri: image.uri,
-          type: image.mimeType || 'image/jpeg',
-          name: image.fileName || 'uploaded_image.jpg'
-        })
-        
-
-        try {
-          let response = await fetch(
-            `http://${ipAddress}:8000/predict/`,
-            {
-              method: 'post',
-              body: formdata,
-              headers: {
-                'Content-Type': 'multipart/form-data',
-              }
-            }
-          );
-
-        let responseJson = await response.json();
-        console.log(responseJson, "responseJson")
-        Alert.alert('Server Response', JSON.stringify(responseJson));
-  
-        } catch (error) {
-              console.error(error);
-              Alert.alert('Server Connection Error', 'Could not connect to the server.');
-        }
-      };
-
-      const testServerConnection = async () => {
-            try {
-              const response = await fetch(`http://${ipAddress}:8000/test-get/`);
-              const json = await response.json();
-              Alert.alert('Server Response', JSON.stringify(json));
-            } catch (error) {
-              console.error(error);
-              Alert.alert('Server Connection Error', 'Could not connect to the server.');
-            }
-          };
-
+const App = () => {
   return (
-    <View
-    style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-      {/* <Button title='upload' onPress={openGallery}></Button> */}
-      <Button title="Pick an Image from Gallery" onPress={pickImageAndSend} />
-      <Button title="Test Server Connection" onPress={testServerConnection} />
-    </View>
+    <>
+      {/* <View style={styles.xpBarContainer}>
+        <XPBar currentXP={70} maxXP={100} />
+      </View> */}
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarActiveTintColor: "green",
+            tabBarInactiveTintColor: "gray",
+            tabBarStyle: {
+              backgroundColor: "black",
+            },
+            tabBarLabelStyle: {
+              fontWeight: "bold",
+              // fontSize: 12
+            },
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              let iconSize = size;
+              let iconColor = color;
+              if (route.name === "Home") {
+                iconName = focused ? "home" : "home-outline";
+              } else if (route.name === "Challenges") {
+                iconName = focused ? "ribbon" : "ribbon-outline";
+              } else if (route.name === "Identify") {
+                iconName = focused ? "leaf" : "leaf-outline";
+                iconSize = focused ? size * 1.5 : size;
+                iconColor = focused ? "limegreen" : "darkgreen";
+              } else if (route.name === "Leaderboard") {
+                iconName = focused ? "podium" : "podium-outline";
+              } else if (route.name === "Profile") {
+                iconName = focused ? "person" : "person-outline";
+              }
+
+              return (
+                <Ionicons name={iconName} size={iconSize} color={iconColor} />
+              );
+            },
+          })}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Challenges" component={ChallengesScreen} />
+          <Tab.Screen name="Identify" component={TakeImageScreen} />
+          <Tab.Screen name="Leaderboard" component={LeaderboardGamesScreen} />
+          <Tab.Screen name="Profile" component={ProfileScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//     xpBarContainer: {
+//       height: 20, 
+//       backgroundColor: '#000', 
+//     },
 
+// });
 
+export default App;
