40c028a47e8a8aa4610c33ddc6666af13db8ff24
Asif2714 <asifabdullah47@gmail.com>
2024-03-19 13:28:51 +0000

    fixed pycache and error E040 for restframework

--- backend/identify_app/admin.py
+++ backend/identify_app/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 
 from .models import User, Plant
+# from rest_framework.authtoken.models import Token
+
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Plant)
+
+
+# class TokenAdmin(admin.ModelAdmin):
+#     list_display = ('key', 'user', 'created')
+#     fields = ('user',)
+#     ordering = ('-created',)
+#     autocomplete_fields = ('user',)
+
+
+# # Commenting this out makes it work
+# admin.site.register(Token, TokenAdmin)
